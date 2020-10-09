928644386f5b881959d922154a64d375cb2142a4
giulcioffi <g.cioffi@arduino.cc>
2020-10-09 09:49:38 +0200

    Rename merged database report into expected-results-database.json

--- .github/database/merge-database-reports.sh
+++ .github/database/merge-database-reports.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if test -f database-reports/full-database-report.json; then
-    rm database-reports/full-database-report.json
+if test -f database-reports/expected-results-database.json; then
+    rm database-reports/expected-results-database.json
 fi
 
 echo "Merging the board-related json files from the database into a single report"
 
-jq -s '.[0].boards=([.[].boards]|flatten)|.[0]' database-reports/*.json >> database-reports/full-database-report.json
+jq -s '.[0].boards=([.[].boards]|flatten)|.[0]' database-reports/*.json >> database-reports/expected-results-database.json
 
-jq 'del(.boards[].sketches[].sizes)' database-reports/full-database-report.json >> database-reports/modified-full-database-report.json
+jq 'del(.boards[].sketches[].sizes)' database-reports/expected-results-database.json >> database-reports/modified-results-database.json
 
-mv database-reports/modified-full-database-report.json database-reports/full-database-report.json
+mv database-reports/modified-results-database.json database-reports/expected-results-database.json
 
 exit 0
